4afd2b103736c38ceaf7cc3726fe90de89da42de
moulyamallesh733 <moulyamallesh733@gmail.com>
2025-12-28 19:54:32 +0530

    Update backend app.py.py

--- backend app.py.py
+++ backend app.py.py
@@ -3,16 +3,17 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/alert', methods=['POST'])
+@app.route('/alert!', methods=['POST'])
 def alert():
     data = request.json
-    lat = data.get('lat')
-    lon = data.get('lon')
+    lattitude = data.get('lattitude')
+    longitude = data.get('longitude')
 
     return jsonify({
-        "message": "Accident detected!",
+        "Message": "Accident detected!",
         "location": f"https://maps.google.com/?q={lat},{lon}"
     })
 
 if __name__ == "__main__":
+
     app.run(debug=True)
